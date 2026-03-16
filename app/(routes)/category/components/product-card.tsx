1bd8e6231f520d93a33f3698e3fb840105f8f81b
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2026-03-15 22:03:09 -0300

    CSS: category label hidden for mobile

--- app/(routes)/category/components/product-card.tsx
+++ app/(routes)/category/components/product-card.tsx
@@ -19,7 +19,7 @@ const ProductCard = (props: ProductCardProps) => {
     <Link href={`/product/${product.slug}`}
       className="relative p-2 transition-all duration-100 rounded-lg hover:shadow-md h-fit w-4/5 sm:w-auto"
     >
-      <div className="absolute flex items-center justify-between gap-3 px-2 z-1 top-4">
+      <div className="absolute hidden sm:flex items-center justify-between gap-3 px-2 z-1 top-4">
         <p className="px-2 py-1 text-xs text-white bg-black rounded-full dark:bg-white dark:text-black w-fit">
           {product.brand}
         </p>
